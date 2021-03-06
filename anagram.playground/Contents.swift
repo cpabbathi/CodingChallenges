e79b73caab75e4c589e568776822262be2d62b0e
Chaithra Pabbathi <chaithrap@gmail.com>
2021-03-06 17:25:51 -0600

    Added several solutions and compared their runtimes.

--- anagram.playground/Contents.swift
+++ anagram.playground/Contents.swift
@@ -12,14 +12,67 @@ extension String {
     }
 }
 
-
 func isAnagram(first: String, second: String) -> Bool {
     first.makeDictionary() == second.makeDictionary()
 }
 
-isAnagram(first: "mothers", second: "thermos")
-isAnagram(first: "mothers", second: "thermo")
-isAnagram(first: "mother", second: "thermos")
-isAnagram(first: "a", second: "")
-isAnagram(first: "Dat", second: "tad")
-isAnagram(first: "A", second: "AA")
+func isAnagramWithSort(first: String, second: String) -> Bool {
+    first.sorted() == second.sorted()
+}
+    
+func isAnagramWithScanAndIncrementOrDecrement(first: String, second: String) -> Bool {
+    guard first.count == second.count else {
+        return false
+    }
+    
+    let first = first.lowercased()
+    let second = second.lowercased()
+    
+    var dictionary:[String.Element: Int] = [:]
+    
+    for i in 0 ..< first.count {
+        dictionary[first[first.index(first.startIndex, offsetBy: i)]] = (dictionary[first[first.index(first.startIndex, offsetBy: i)]] ?? 0) + 1
+        dictionary[second[second.index(second.startIndex, offsetBy: i)]] = (dictionary[second[second.index(second.startIndex, offsetBy: i)]] ?? 0) - 1
+    }
+    
+    return dictionary.allSatisfy { (dictionaryElement) -> Bool in
+        dictionaryElement.value == 0
+    }
+}
+ 
+var startTime = Date()
+isAnagramWithSort(first: "mothers", second: "thermos") == true
+var endTime = Date()
+print(String(format: "%.6f", endTime.timeIntervalSince(startTime)))
+
+startTime = Date()
+isAnagram(first: "mothers", second: "thermo") == false
+endTime = Date()
+print(String(format: "%.6f", endTime.timeIntervalSince(startTime)))
+
+startTime = Date()
+isAnagram(first: "mother", second: "thermos") == false
+endTime = Date()
+print(String(format: "%.6f", endTime.timeIntervalSince(startTime)))
+
+startTime = Date()
+isAnagram(first: "a", second: "") == false
+endTime = Date()
+print(String(format: "%.6f", endTime.timeIntervalSince(startTime)))
+
+startTime = Date()
+isAnagram(first: "Dat", second: "tad") == true
+endTime = Date()
+print(String(format: "%.6f", endTime.timeIntervalSince(startTime)))
+
+startTime = Date()
+isAnagram(first: "A", second: "AA") == false
+endTime = Date()
+print(String(format: "%.6f", endTime.timeIntervalSince(startTime)))
+
+startTime = Date()
+isAnagram(first: "adkfjaeioadfaagasdfasdgaaagasdasdfaadkfjaeioadfaagasdfasdgaaagasdasdfasdfasdfanjadfadfadsdfasdfanjadfadfadadkfjaeioadfaagasdfasdgaaagasdasdfaadkfjaeioadfaagasdfasdgaaagasdasdfasdfasdfanjadfadfadsdfasdfanjadfadfadadkfjaeioadfaagasdfasdgaaagasdasdfaadkfjaeioadfaagasdfasdgaaagasdasdfasdfasdfanjadfadfadsdfasdfanjadfadfadadkfjaeioadfaagasdfasdgaaagasdasdfaadkfjaeioadfaagasdfasdgaaagasdasdfasdfasdfanjadfadfadsdfasdfanjadfadfad", second: "adfasdgaaagasdasdfasioadadfadfasdfanjfadfadkfjaadfasdgaaagasdasdfasioadadfadfasdfanjfadfadkfjaeagasdeagasdadkfjaeioadfaagasdfasdgaaagasdasdfaadkfjaeioadfaagasdfasdgaaagasdasdfasdfasdfanjadfadfadsdfasdfanjadfadfadadkfjaeioadfaagasdfasdgaaagasdasdfaadkfjaeioadfaagasdfasdgaaagasdasdfasdfasdfanjadfadfadsdfasdfanjadfadfadadkfjaeioadfaagasdfasdgaaagasdasdfaadkfjaeioadfaagasdfasdgaaagasdasdfasdfasdfanjadfadfadsdfasdfanjadfadfad") == true
+endTime = Date()
+print(String(format: "%.6f", endTime.timeIntervalSince(startTime)))
+
+print("adfasdgaaagasdasdfasioadadfadfasdfanjfadfadkfjaadfasdgaaagasdasdfasioadadfadfasdfanjfadfadkfjaeagasdeagasdadkfjaeioadfaagasdfasdgaaagasdasdfaadkfjaeioadfaagasdfasdgaaagasdasdfasdfasdfanjadfadfadsdfasdfanjadfadfadadkfjaeioadfaagasdfasdgaaagasdasdfaadkfjaeioadfaagasdfasdgaaagasdasdfasdfasdfanjadfadfadsdfasdfanjadfadfadadkfjaeioadfaagasdfasdgaaagasdasdfaadkfjaeioadfaagasdfasdgaaagasdasdfasdfasdfanjadfadfadsdfasdfanjadfadfad".count)
