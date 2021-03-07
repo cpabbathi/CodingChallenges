e0811449d7cfcef34a0d662d701a5462973e9399
Chaithra Pabbathi <chaithrap@gmail.com>
2021-03-07 16:24:56 -0600

    Added the first solution for the problem.
    https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/

---
+++ LeetCode/378 Kth Smallest Element in a Sorted Matrix.playground/Contents.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+class Solution {
+    var array: [Int] = []
+    
+    func kthSmallest(_ matrix: [[Int]], _ k: Int) -> Int {
+        array.append(contentsOf: matrix[0])
+        
+        for row in 1 ..< matrix.count {
+            for column in 0 ..< matrix[row].count {
+                let index = insert(element: matrix[row][column])
+                if column == 0 && index >= k {
+                    return array[k-1]
+                }
+            }
+        }
+        
+        return array[k-1]
+    }
+    
+    private func insert(element: Int) -> Int {
+        if array.count == 0 || element < array[0] {
+            array.insert(element, at: 0)
+            return 0
+        } else if element >= array.last! {
+            array.append(element)
+            return array.count - 1
+        } else {
+            var from = 0
+            var to = array.count - 1
+            
+            while from <= to {
+                let middle = from + (to - from) / 2
+                 
+                if element >= array[middle] && element < array[middle + 1] {
+                    array.insert(element, at: middle + 1)
+                    return middle + 1
+                } else if element < array[middle] {
+                    to = middle
+                } else {
+                    from = middle
+                }
+            }
+        }
+        
+        return 0
+    }
+}
+
+Solution().kthSmallest([[1,5,9,30],[10,11,13,31],[12,13,15,32],[14,15,16,34]], 16)
